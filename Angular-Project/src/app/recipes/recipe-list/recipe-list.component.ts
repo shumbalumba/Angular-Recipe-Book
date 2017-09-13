ad798e09b7838b43531e99e618b9d472d87fd6c3
shumbalumba <aagnisb@gmail.com>
2017-09-13 16:36:50 +0100

    added test recipe list and description area

--- Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('First Recipe',
+    'This is a test recipe',
+    'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'),
+    new Recipe('First Recipe',
+    'This is a test recipe',
+    'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb')
+  ];
 
   constructor() { }
 
